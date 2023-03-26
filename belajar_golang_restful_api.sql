d61bcdc10267ed4934d6ed38fbf00c69e9a054bc
Andromeda <andromeda951@gmail.com>
2023-03-26 23:04:05 +0700

    Membuat Integration Test

--- belajar_golang_restful_api.sql
+++ belajar_golang_restful_api.sql
@@ -12,3 +12,18 @@ CREATE TABLE category (
 SHOW TABLES;
 DESC category;
 SELECT * FROM category;
+
+SHOW DATABASES;
+
+CREATE DATABASE belajar_golang_restful_api_test;
+
+USE belajar_golang_restful_api_test;
+
+CREATE TABLE category (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(200) NOT NULL
+)ENGINE=InnoDB;
+
+SHOW TABLES;
+
+SELECT * FROM category;	
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"andromeda/belajar-golang-restful-api/app"
 	"andromeda/belajar-golang-restful-api/controller"
-	"andromeda/belajar-golang-restful-api/exception"
 	"andromeda/belajar-golang-restful-api/helper"
 	"andromeda/belajar-golang-restful-api/middleware"
 	"andromeda/belajar-golang-restful-api/repository"
@@ -14,7 +13,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -25,18 +23,10 @@ func main() {
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
 
-	router := httprouter.New()
-
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
-		Addr: "localhost:3000",
+		Addr:    "localhost:3000",
 		Handler: middleware.NewAuthMiddleware(router),
 	}
 
